42efa17c5bd2942477f9958562192ec9980c4d72
Kennedy Musau <kennedy.musauu@gmail.com>
2023-05-27 14:19:46 +0300

    Completed task

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((questions) => setQuestions(questions));
+  }, []);
+
+  function AddQuestion(question) {
+    setQuestions([...questions, question]);
+  }
+
+  function deleteQuestion(deltedQuestion) {
+    const updatedQuestionList = questions.filter(
+      (question) => question.id !== deltedQuestion.id
+    );
+    setQuestions(updatedQuestionList);
+  }
+
+  function updatedCorrectIndex(updatedQuestion) {
+    const updatedAnswerIndex = questions.map((question) => {
+      if (question.id === updatedQuestion.id) {
+        return updatedQuestion;
+      } else {
+        return question;
+      }
+    });
+    setQuestions(updatedAnswerIndex);
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onAddQuestion={AddQuestion} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          onDeleteQuestion={deleteQuestion}
+          onCorrectIndexUpdate={updatedCorrectIndex}
+        />
+      )}
     </main>
   );
 }
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, DeleteQuestion, onCorrectIndexUpdate }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,38 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function deleteQuestion() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() => DeleteQuestion(question));
+  }
+
+  function updateCorectIndex(e) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        correctIndex: e.target.value,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((questionCorrectIndexUpdate) => onCorrectIndexUpdate(questionCorrectIndexUpdate));
+  }
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={updateCorectIndex} defaultValue={correctIndex}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={deleteQuestion}>Delete Question</button>
     </li>
   );
 }
